19a3da8611049610ec9e6bd830cef278606d4331
Alexhan <46094938+hanhyeonkyu@users.noreply.github.com>
2022-03-11 09:09:08 +0900

    pdftotxt update

--- pdf_to_txt/main.py
+++ pdf_to_txt/main.py
@@ -1,16 +1,20 @@
-from multiprocessing import Process, Queue, Pool
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+import os
+# import time
+from io import StringIO
+from multiprocessing import Pool, cpu_count, freeze_support
+
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
+from pdfminer.pdfinterp import PDFPageInterpreter, PDFResourceManager
 from pdfminer.pdfpage import PDFPage
-from io import StringIO
-import os, time
+
 
 def get_file_names(path):
     file_list = os.listdir(path)
     return file_list
 
-def get_file_paths( input_path, file_names, replacer):
+
+def get_file_paths(input_path, file_names, replacer):
     if(replacer == 'txt'):
         x = []
         for i, v in enumerate(file_names):
@@ -22,20 +26,21 @@ def get_file_paths( input_path, file_names, replacer):
             x.insert(i, input_path + v)
         return x
 
+
 def convert_pdf_to_txt(path):
     rsrcmgr = PDFResourceManager()
     retstr = StringIO()
-    codec = 'utf-8'
+    # codec = 'utf-8'
     laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
     fp = open(path, 'rb')
     interpreter = PDFPageInterpreter(rsrcmgr, device)
     password = ""
     maxpages = 0
     caching = True
-    pagenos=set()
+    pagenos = set()
 
-    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching, check_extractable=True):
         interpreter.process_page(page)
 
     text = retstr.getvalue()
@@ -45,26 +50,31 @@ def convert_pdf_to_txt(path):
     retstr.close()
     return text
 
+
 def multiprocessing_work(paths):
-    return list(Pool(processes= 4).map(convert_pdf_to_txt, paths))
+    return Pool(processes=cpu_count()).map(convert_pdf_to_txt, paths)
+
 
 def save_text_files(output_path, path_list, string_list):
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
     for i, v in enumerate(path_list):
         text_file = open(v, "w")
         text_file.write(string_list[i])
         text_file.close()
 
+
 def main(input_path, output_path):
-# def main():
+
     # start_time = time.time()
-    # input_path = "./files/"
-    # output_path = "./output/"
     file_name_list = get_file_names(input_path)
     file_input_path_list = get_file_paths(input_path, file_name_list, "input")
-    # file_string_list = list( map( convert_pdf_to_txt, file_input_path_list ) )
-    file_string_list = multiprocessing_work(file_input_path_list)
     file_output_path_list = get_file_paths(output_path, file_name_list, "txt")
+    file_string_list = multiprocessing_work(file_input_path_list)
     save_text_files(output_path, file_output_path_list, file_string_list)
     # print("--- %s seconds ---" % (time.time() - start_time))
 
-# main()
+
+# if __name__ == '__main__':
+#     freeze_support()
+#     main("./files/", "./output/")
---
+++ readme.md
@@ -0,0 +1,28 @@
+## pdf to text program
+
+### USAGE
+
+1. with terminal
+```zsh
+cd pdf_to_txt
+# open main.py
+# __name__ == __main__ part sentence activate
+# write path where you want and then
+python main.py
+```
+2. with pyqt5
+```zsh
+cd pdf_to_txt
+python run_layout.py
+# write path where you want
+# click convert
+```
+3. with standalone
+```zsh
+cd pdf_to_txt
+pyinstaller -F run_layout.py
+cd dist
+./run_layout
+# write path where you want
+# click convert
+```
